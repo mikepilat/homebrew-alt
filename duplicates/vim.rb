ed4afb85175c0f2c4e5e0dc1556a2e1f8b9f7c6e
Michael Pilat <mike@mikepilat.com>
2012-07-24 09:50:05 -0500

    Update to vim 7.3.608.

--- duplicates/vim.rb
+++ duplicates/vim.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Vim < Formula
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  url 'https://vim.googlecode.com/hg/', :revision => '273fbf501965'
-  version '7.3.515'
+  url 'https://vim.googlecode.com/hg/', :revision => '580391c2f925'
+  version '7.3.608'
   homepage 'http://www.vim.org/'
 
   head 'https://vim.googlecode.com/hg/'
